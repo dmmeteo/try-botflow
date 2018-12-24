544c431e8a6e92a89b17f5ac7ba9ffedaca581c9
Dmitry Sichkar <dmmeteo@gmail.com>
2018-12-24 11:44:54 +0200

    Init bot app

---
+++ bot.py
@@ -0,0 +1,18 @@
+from botflow import *
+from aiohttp import web
+
+
+p = Pipe(
+
+    {"msg":"hello world!"}
+)
+
+
+app = web.Application()
+app.add_routes([
+    web.get('/', p.aiohttp_json_handle)
+])
+
+Bot.run_app(app)
+#BotFlow start web server http://0.0.0.0:8080
+
