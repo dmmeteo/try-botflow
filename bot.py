0d9187031577d9eed07ee11e37ece7a5d042c9a1
Dmitry Sichkar <dmmeteo@gmail.com>
2019-01-09 01:45:44 +0200

    🔨 refactor Bot

--- bot.py
+++ bot.py
@@ -1,16 +1,26 @@
+#!/usr/bin/env python
 from botflow import Pipe, Bot
 from aiohttp import web
 
 
 p = Pipe(
-    {"msg": "hello world!"}
+    {"msg": "hello world!"},
+    {"error": "Botflow sucks!"},
+    {"error": "Botflow sucks2!"}
 )
 
 
-app = web.Application
-app.add_routes([
-    web.get('/', p.aiohttp_json_handle)
-])
+def create_app():
+    app = web.Application()
 
-Bot.run_app(app)
-#BotFlow start web server http://0.0.0.0:8080
+    app.add_routes([
+        web.get('/', p.aiohttp_json_handle)
+    ])
+
+    return app
+
+
+if __name__ == '__main__':
+    # BotFlow start web server http://0.0.0.0:8080
+    app = create_app()
+    Bot.run_app(app)
